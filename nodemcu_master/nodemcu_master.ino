250189d44d207be8b5c78258c0b8e1ec962cec9a
jaegeon <zezaeoh@gmail.com>
2019-05-11 10:47:53 +0900

    add enum status code

--- nodemcu_master/nodemcu_master.ino
+++ nodemcu_master/nodemcu_master.ino
@@ -21,10 +21,27 @@
 #define INF 10000
 #define ON 1
 #define OFF 0
+
+//for state control
+#define ERROR_CONTROL -1
+#define START_DRIVING 0
+#define WHILE_DRIVING 1
+#define END_DRIVING 2
+#define CHECK_EMERGENCY 3
+#define WAIT_FOR_BUTTON_INPUT 5
+#define END_EMERGENCY 6
+#define DISPLAY_EMERGENCY_2_STATUS 8
+#define WAIT_BUTTON_UNTILE_BATTERY_DIE 10
+
+#define SEND_EMERGENCY_1_STATUS_TO_SERVER 4
+#define SEND_EMERGENCY_2_STATUS_TO_SERVER 7
+#define SEND_DRIVING_END_STATUS_TO_SERVER 9
+
+
 const static char* DEVICE_ID = "sc13150";
 
-const static char* SSIDNAME[] = { "PPL 2.4 GHz", "iPhone_12"};
-const static char* PASSWORD[] = { "pplppl2938", "223456778!"};
+const static char* SSIDNAME = "sn";
+const static char* PASSWORD = "pw";
 const static int WIFI_NUM = 2;
 
 // for display
@@ -138,7 +155,7 @@ void controlSpeaker(int speakerStatus) {
   else
     digitalWrite(SPEAKER_OUT, LOW);
 }
-//
+
 //bool isButtonPressed(){
 //  if(digitalRead(BUTTON_CHECK))
 //    return true;
@@ -150,20 +167,16 @@ void setup () {
   Serial.begin(115200);
   countSerial.begin(9600);
   Wire.begin();
-//  WiFi.begin(SSIDNAME[0], PASSWORD[0]);
+  WiFi.begin(SSIDNAME, PASSWORD);
   lcd.init();
   lcd.backlight();
   lcd.display();
   pinMode(BUTTON_CHECK, INPUT);
   pinMode(SPEAKER_OUT, OUTPUT);
   
-  int t = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     displayToLcd(NOT_CONNECTED, NOT_CONNECTED_LEN, 0);
-    t = 1 - t;
-    WiFi.begin(SSIDNAME[t], PASSWORD[t]);
-    delay(10000);
   }
 
   displayToLcd(CONNECTED, CONNECTED_LEN, 0);
@@ -177,19 +190,19 @@ void setup () {
   displayToLcd(DRIVE_MODE_START_1, DRIVE_MODE_START_1_LEN, DRIVE_MODE_START_2, DRIVE_MODE_START_2_LEN);
   delay(5000);
 
-  status_code = 0;
+  status_code = START_DRIVING;
 }
 
 void loop() {
   switch (status_code) {
-    case -1: // error (maybe network)
+    case ERROR_CONTROL: // error (maybe network)
       displayToLcd(NETWORK_ERROR_DISP_1, NETWORK_ERROR_DISP_1_LEN, NETWORK_ERROR_DISP_2, NETWORK_ERROR_DISP_2_LEN);
       delay(5000);
       if (requestGET(DEVICE_ID))
         status_code = status_code_stack;
       break;
 
-    case 0: // driviing start
+    case START_DRIVING: // driving start
       displayToLcd(CURRENT_CHILD, CURRENT_CHILD_LEN, 0);
       sprintf(PARAM_CHAR_BUFFER, "%s/start", DEVICE_ID);
 
@@ -199,14 +212,14 @@ void loop() {
         countSerial.write(1);
       } else {
         status_code_stack = status_code;
-        status_code = -1;
+        status_code = ERROR_CONTROL;
       }
       break;
 
-    case 1: // while driving
+    case WHILE_DRIVING: // while driving
       if (!isPowerOn()) {
         // check power: if power off then, status_code = 2;
-        status_code = 2;
+        status_code = END_DRIVING;
       } else {
         if (countSerial.available() > 0) {
           tmp  = countSerial.read();
@@ -217,90 +230,90 @@ void loop() {
 
             if (!requestGET(PARAM_CHAR_BUFFER)) {
               status_code_stack = status_code;
-              status_code = -1;
+              status_code = ERROR_CONTROL;
             }
           }
         }
       }
       break;
 
-    case 2: // driving end
+    case END_DRIVING: // driving end
       displayToLcd(DRIVE_MODE_END, DRIVE_MODE_END_LEN, 0);
-      status_code = 9;
+      status_code = SEND_DRIVING_END_STATUS_TO_SERVER;
       delay(5000);
       break;
 
-    case 3: // check emergency
+    case CHECK_EMERGENCY: // check emergency
       if (child_cnt > 0) {
         displayToLcd(EMERGE_1_DISP_1, EMERGE_1_DISP_1_LEN, EMERGE_1_DISP_2, EMERGE_1_DISP_2_LEN);
         // turn on the speaker
         controlSpeaker(ON);
-        status_code = 4;
+        status_code = SEND_EMERGENCY_1_STATUS_TO_SERVER;
       } else {
         displayToLcd(NORMAL_END_1, NORMAL_END_1_LEN, NORMAL_END_2, NORMAL_END_2_LEN);
         status_code = INF;
       }
       break;
 
-    case 4: // send emergency status to server
+    case SEND_EMERGENCY_1_STATUS_TO_SERVER: // send emergency status to server
       sprintf(PARAM_CHAR_BUFFER, "%s/emerg/%d", DEVICE_ID, EMERGE_1);
 
       if (!requestGET(PARAM_CHAR_BUFFER)) {
-        status_code_stack = 3;
-        status_code = -1;
+        status_code_stack = CHECK_EMERGENCY;
+        status_code = ERROR_CONTROL;
       } else {
         previousMillis = millis();
-        status_code = 5;
+        status_code = WAIT_FOR_BUTTON_INPUT;
       }
       break;
 
-    case 5: // wait for button input, check timeout
+    case WAIT_FOR_BUTTON_INPUT: // wait for button input, check timeout
       currentMillis = millis();
       if (currentMillis - previousMillis >= INTERVAL) { // timeout
         displayToLcd(TIMEOUT_DISP, TIMEOUT_DISP_LEN, 0);
-        status_code = 7;
+        status_code = SEND_EMERGENCY_2_STATUS_TO_SERVER;
       } else {
         // wait button input
         if (0) { // button pressed
           // stop the speaker
           controlSpeaker(OFF);
-          status_code = 6;
+          status_code = END_EMERGENCY;
         }
       }
       break;
 
-    case 6: // emergency end
+    case END_EMERGENCY: // emergency end
       displayToLcd(EMERGE_END_DISP_1, EMERGE_END_DISP_1_LEN, EMERGE_END_DISP_2, EMERGE_END_DISP_2_LEN);
       status_code = INF;
       break;
 
-    case 7:
+    case SEND_EMERGENCY_2_STATUS_TO_SERVER:
       sprintf(PARAM_CHAR_BUFFER, "%s/emerg/%d", DEVICE_ID, EMERGE_2);
 
       if (!requestGET(PARAM_CHAR_BUFFER)) {
         status_code_stack = status_code;
-        status_code = -1;
+        status_code = ERROR_CONTROL;
       } else {
-        status_code = 8;
+        status_code = DISPLAY_EMERGENCY_2_STATUS;
       }
       break;
 
-    case 8:
+    case DISPLAY_EMERGENCY_2_STATUS:
       displayToLcd(EMERGE_2_DISP_1, EMERGE_2_DISP_1_LEN, EMERGE_2_DISP_2, EMERGE_2_DISP_2_LEN);
-      status_code = 10;
+      status_code = WAIT_BUTTON_UNTILE_BATTERY_DIE;
       break;
 
-    case 9:
+    case SEND_DRIVING_END_STATUS_TO_SERVER:
       sprintf(PARAM_CHAR_BUFFER, "%s/end", DEVICE_ID);
 
       if (!requestGET(PARAM_CHAR_BUFFER)) {
         status_code_stack = status_code;
-        status_code = -1;
+        status_code = ERROR_CONTROL;
       } else
-        status_code = 3;
+        status_code = CHECK_EMERGENCY;
       break;
 
-    case 10: // wait for button until battery burn out
+    case WAIT_BUTTON_UNTILE_BATTERY_DIE: // wait for button until battery burn out
       // wait button input
       if (0) { // button Pressed
         // stop the speaker
